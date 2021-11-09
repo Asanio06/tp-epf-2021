5a64922c6f138cbedf3783d554d4d87f2278d6fa
Nina DEI TOS <nina.deitos@epfedu.fr>
2021-11-09 12:05:16 +0100

    exo7

--- contracts/ElectionCreator.sol
+++ contracts/ElectionCreator.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.6.8;
 import "../contracts/Election.sol";
 
 contract ElectionCreator {
+    uint public electionsCount;
+    mapping(uint => address) public electionList;
+
     // Creates an election contract and emits the according event
     function newElection(string calldata _name) external {
+        electionList[electionsCount] = address (new Election(_name));
+        electionsCount++;
     }
 }
